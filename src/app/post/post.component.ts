6cf131299f13f84561c88271c8f1120ff41f7809
ramprit <Rampritsahani@outlook.in>
2018-07-08 19:44:31 +0530

    Post loadong indicator added

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -5,17 +5,21 @@ import { Post } from "@app/post/post.model";
 @Component({
   selector: "app-post",
   templateUrl: "./post.component.html",
-  styleUrls: ["./post.component.scss"]
+  styleUrls: ["./post.component.scss"],
+  preserveWhitespaces:true
 })
 export class PostComponent implements OnInit {
   posts: Post[];
+  isLoading:boolean;
   constructor(private postService: PostService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     setTimeout(() => {
       this.postService.getPosts().subscribe(post => {
         this.posts = post;
+        this.isLoading = false;
       });
-    }, 100);
+    }, 2000);
   }
 }
--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
 import { PostRoutingModule } from '@app/post/post.routing';
+import { SharedModule } from '@app/shared';
 
 @NgModule({
   imports: [
-    CommonModule,PostRoutingModule
+    CommonModule,PostRoutingModule , SharedModule
   ],
   declarations: [PostComponent]
 })
